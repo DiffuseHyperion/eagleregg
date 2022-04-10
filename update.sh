7318817a5b18e56d422306c40a0915c078d09ca5
DiffuseHyperion <xieyingjie08@gmail.com>
2022-04-10 12:02:06 +0800

    add website dir, also change zip file names

--- update.sh
+++ update.sh
@@ -5,8 +5,9 @@ unzip stable-download-new.zip
 
 rm bukkit_command.zip
 rm bungee_command.zip
-zip -r -j bukkit-command.zip  java/bukkit_command
-zip -r -j bungee-command.zip  java/bungee_command
+zip -r -j bukkit.zip  java/bukkit_command
+zip -r -j bungee.zip  java/bungee_command
+zip -r -j website.zip  web
 
 rm -r java
 rm -r web
